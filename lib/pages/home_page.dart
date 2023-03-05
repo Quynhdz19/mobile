1b7e0eedcb2832b9b36474e5f5dc5832bd9ce52c
quynhlx <quynhlx@kaopiz.com>
2023-03-05 19:14:08 +0700

    login page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Hompage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title:Text("hompae"),
+      ),
+    );
+  }
+}

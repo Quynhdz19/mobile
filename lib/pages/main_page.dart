656f53d2afa978a4027077be46f688052e74fa1c
nguyenloan <20020114@vnu.edu.vn>
2023-03-09 00:32:00 +0700

    create pages

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:mobile_front_end/pages/home_page.dart';
+import 'package:mobile_front_end/pages/learn_page.dart';
+import 'package:mobile_front_end/pages/profile_page.dart';
+import 'package:mobile_front_end/pages/settings_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  int _selectedIndex = 0;
+
+  static final List<Widget> navigationBar = <Widget>[
+    HomePage(),
+    LearnPage(),
+    SettingsPage(),
+    ProfilePage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Welcome, An")),
+      body: Center(child: navigationBar.elementAt(_selectedIndex),),
+    bottomNavigationBar: GNav(
+            backgroundColor: Colors.lightBlueAccent,
+            color: Colors.white,
+            gap: 8,
+            // onTabChange: (index) {
+            //   print(index);
+            // },
+            tabs: [
+              GButton(icon: Icons.home, text: "Home",),
+              GButton(icon: Icons.menu_book_outlined, text: "Learn",),
+              GButton(icon: Icons.settings, text: "Settings",),
+              GButton(icon: Icons.person, text: "Profile",),
+            ],
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+          ),
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:mobile_front_end/auth/login.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(duration: 1000,
+        splash: Icons.home,
+        nextScreen: LoginPage(),
+        splashTransition: SplashTransition.fadeTransition,
+        // pageTransitionType: PageTransitionType.scale,
+        backgroundColor: Colors.blue);
+  }
+}

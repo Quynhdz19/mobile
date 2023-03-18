1a3894f6584ae5a3b38f8f8e9ba2ec52dd93bcc1
thuyquynh2508 <thuyquynh404@gmail.com>
2023-03-18 10:49:40 +0700

    quiz game: welcome page and quiz page UI

--- lib/pages/games/quizGame/quizPage/quiz_page.dart
+++ lib/pages/games/quizGame/quizPage/quiz_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_front_end/pages/games/quizGamePage/components/quiz_body.dart';
+import 'package:mobile_front_end/pages/games/quizGame/quizPage/components/quiz_body.dart';
 import 'package:mobile_front_end/utils/constants.dart';
 
 class QuizGamePage extends StatelessWidget {

13ae8acd64f1e5b8837b5e75508e8d959482686e
thuyquynh2508 <thuyquynh404@gmail.com>
2023-04-15 17:55:42 +0700

    change the application laucher icon and splash screen in android

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
 
       onGenerateRoute: router.generateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
-      initialRoute: routers.LoginPage,
+      initialRoute: routers.LearnTopic,
 
       // routers.QuizGameWelcomePage,
       // home: WelcomePage(),

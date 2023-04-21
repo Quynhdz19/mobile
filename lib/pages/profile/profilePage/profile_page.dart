8c4643bd9087865eb04ef177d629660dba584db5
thulexuan <thudtyl0210@gmail.com>
2023-04-22 04:35:00 +0700

    modify ranking page UI

--- lib/pages/profile/profilePage/profile_page.dart
+++ lib/pages/profile/profilePage/profile_page.dart
@@ -217,6 +217,13 @@ class _ProfilePageState extends State<ProfilePage> {
                   },
                 ),
                 ProfileMenuItem(
+                  title: 'Ranking'.tr,
+                  icon: Icons.assessment,
+                  onPress: () {
+                    _navigationService.navigateTo(routes.RankingPage);
+                  },
+                ),
+                ProfileMenuItem(
                   title: 'feedback'.tr,
                   icon: Icons.feedback,
                   onPress: () {},

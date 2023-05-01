0ac371a0bcc9c10ad9b7588489be629105f135ae
thulexuan <thudtyl0210@gmail.com>
2023-05-01 22:59:31 +0700

    pull develop

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class MyApp extends StatelessWidget {
 
       onGenerateRoute: router.generateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
-      initialRoute: routers.LearnNewWord,
+      initialRoute: routers.LoginPage,
 
 
       // routers.QuizGameWelcomePage,

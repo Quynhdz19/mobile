5a3df0eb8ba7fcff656d230bf3619ebfc8cb1f19
quynhlx <quynhlx@kaopiz.com>
2023-05-16 23:17:30 +0700

    git add pages favorite

--- lib/pages/home/allFavoritesPage/components/favorite_topic_box.dart
+++ lib/pages/home/allFavoritesPage/components/favorite_topic_box.dart
@@ -1,76 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_front_end/pages/learn/vocabByTopic/newWordPage/new_word_page.dart';
 import 'package:mobile_front_end/utils/constants.dart';
 
 class FavoriteTopicBox extends StatelessWidget {
-  const FavoriteTopicBox({Key? key, required this.topic, this.onTab}) : super(key: key);
-/// tesjdckc
+  const FavoriteTopicBox({Key? key, required this.topic}) : super(key: key);
+  /// tesjdckc
   final topic;
-  final GestureTapCallback? onTab;
   // test pull origin
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTab,
+      onTap: ()  {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) =>  NewWordPage(id: topic['id'])),
+        );
+      },
       child: Container(
-        width: 200,
-        height: 220,
+        width: 150,
+        // height: 220,
         decoration: BoxDecoration(
             color: lightBackgroundColor,
             borderRadius: BorderRadius.circular(10),
             boxShadow: [
               BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
+                  color: Colors.grey.withOpacity(0.3),
                   spreadRadius: 1,
                   blurRadius: 1,
                   offset: const Offset(1, 1))
             ]),
-          child: Column(
-            children: [
-              Container(
-                // width: 200,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.white,
-                  image: DecorationImage(
-                      image: AssetImage(topic["image"]),
-                      fit: BoxFit.cover
-                  ),
+        child: Column(
+          children: [
+            Container(
+              // width: 200,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                image: DecorationImage(
+                    image: NetworkImage(topic['image']),
+                    fit: BoxFit.cover
                 ),
               ),
-              const SizedBox(
-                height: 10,
-              ),
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:[
-                      Text(
-                        topic["name"],
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.titleLarge,
-                      ),
-                      Text(
-                        topic["description"],
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "${topic["word"]} word",
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      )
-                    ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:[
+                  Text(
+                    topic["name"],
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                ),
-            ],
-          ),
+
+                ],
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
--- lib/pages/home/homePage/components/favorites_list.dart
+++ lib/pages/home/homePage/components/favorites_list.dart
@@ -1,12 +1,70 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_front_end/pages/home/allFavoritesPage/all_favorites_page.dart';
+import 'package:mobile_front_end/pages/profile/favoritePage/favorite_page.dart';
 import 'package:mobile_front_end/utils/data/recomentopic_data.dart';
 import 'package:mobile_front_end/pages/home/allFavoritesPage/components/favorite_topic_box.dart';
 import 'package:mobile_front_end/pages/home/allFavoritesPage/components/nofavorite_box.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class FavoritesList extends StatelessWidget {
+class FavoritesList extends StatefulWidget {
   const FavoritesList({Key? key}) : super(key: key);
+
+  @override
+  State<FavoritesList> createState() => _FavoritesListState();
+}
+
+class _FavoritesListState extends State<FavoritesList> with TickerProviderStateMixin {
+  List arrayFavoriteTopics = [];
+
+  Future<void> getScoreUser()  async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('email', isEqualTo: prefs.getString('email')) // add your condition here
+        .get();
+    final Object? data =
+    snapshot.docs.isNotEmpty ? snapshot.docs.first.data() : {};
+    setState(() {
+      arrayFavoriteTopics = data != null && data is Map<String, dynamic> ? data['favorites'] : []; // do không đổi được tên trên firebase nên để vậy
+
+    });
+
+  }
+
+  List dataTopics = [];
+
+
+  Future<void> getDataTopics() async {
+    var values = arrayFavoriteTopics;
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('topics')
+          .where('id', whereIn: values)
+          .get();
+
+      querySnapshot.docs.forEach((doc) {
+        dataTopics.add(doc.data());
+      });
+    } catch (e) {
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future<void> init() async {
+    await getScoreUser();
+    await getDataTopics();
+    setState(() {
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +79,7 @@ class FavoritesList extends StatelessWidget {
                 'my_favorites'.tr,
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
-              favorites.length < 3
+              dataTopics.length < 3
                   ? Container(
                 height: 16,
               )
@@ -30,7 +88,7 @@ class FavoritesList extends StatelessWidget {
                   Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => const AllFavoritesPage()));
+                          builder: (context) => const FavoritePage()));
                 },
                 style: Theme.of(context).textButtonTheme.style,
                 child: const Text(
@@ -41,26 +99,23 @@ class FavoritesList extends StatelessWidget {
             ],
           ),
         ),
-        favorites.isEmpty
+        dataTopics.isEmpty
             ? const NoFavoriteBox()
             : SingleChildScrollView(
-                padding: const EdgeInsets.fromLTRB(15, 5, 15, 10),
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(
-                    favorites.length,
-                    (index) => Container(
-                      margin: const EdgeInsets.only(right: 15),
-                      child: FavoriteTopicBox(
-                        topic: favorites[index],
-                        onTab: () {
-                          print(index);
-                        },
-                      ),
-                    ),
-                  ),
+          padding: const EdgeInsets.fromLTRB(15, 5, 15, 10),
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(
+              dataTopics.length,
+                  (index) => Container(
+                margin: const EdgeInsets.only(right: 15),
+                child: FavoriteTopicBox(
+                  topic: dataTopics[index],
                 ),
               ),
+            ),
+          ),
+        ),
       ],
     );
   }
